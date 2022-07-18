e4540dbb73975693e122589d0d183cf6bee48694
Taichi-Akase <105829922+Taichi-Akase@users.noreply.github.com>
2022-07-18 23:38:44 +0900

    Add files via upload

---
+++ Source_Code/GenomeDownload.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+rm -r ./Genome_hg38
+mkdir ./Genome_hg38
+wget "https://drive.google.com/uc?export=download&id=1uyoUlNtZyJOaUIFMNi2MPMyEJ81B0--9" -O ./Genome_hg38/hg38.fa.fai
+wget --load-cookies /tmp/cookies.txt "https://drive.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://drive.google.com/uc?export=download&id=1v-bfoq-jXYj2iww91ucwBFvQXPpUy24G' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1v-bfoq-jXYj2iww91ucwBFvQXPpUy24G" -O ./Genome_hg38/hg38.fa && rm -rf /tmp/cookies.txt
