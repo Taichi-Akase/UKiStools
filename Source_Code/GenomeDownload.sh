38ca6f1dfab9a6a1cdca3da55dccd290d5a1f8b5
Taichi-Akase <105829922+Taichi-Akase@users.noreply.github.com>
2023-10-18 19:14:41 +0900

    Update GenomeDownload.sh

--- Source_Code/GenomeDownload.sh
+++ Source_Code/GenomeDownload.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir ./Genome_hg38
-wget -nc "https://drive.google.com/uc?export=download&id=1uyoUlNtZyJOaUIFMNi2MPMyEJ81B0--9" -O ./Genome_hg38/hg38.fa.fai
-wget -nc --load-cookies /tmp/cookies.txt "https://drive.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://drive.google.com/uc?export=download&id=1v-bfoq-jXYj2iww91ucwBFvQXPpUy24G' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1v-bfoq-jXYj2iww91ucwBFvQXPpUy24G" -O ./Genome_hg38/hg38.fa && rm -rf /tmp/cookies.txt
+wget -nc "https://drive.google.com/uc?export=download&id=10V8yNiLlFBmFbjXo7nc2Qc66pcUCW2nY" -O ./Genome_hg38/hg38.fa.fai
+wget -nc --load-cookies /tmp/cookies.txt "https://drive.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://drive.google.com/uc?export=download&id=10VV6hPNXbq_8Futvw-2IP0JE5IYxXF6l' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=10VV6hPNXbq_8Futvw-2IP0JE5IYxXF6l" -O ./Genome_hg38/hg38.fa && rm -rf /tmp/cookies.txt
